270b8f8d57af171f7c550001e3921bff96e803ab
alloctor <yuanchang.xu@outlook.com>
2023-06-04 09:31:10 +0800

    FEAT: add neosolaried theme

--- plugins/user.lua
+++ plugins/user.lua
@@ -14,6 +14,23 @@ return {
   {
     'Mofiqul/vscode.nvim',
     lazy = false,
+  },
+  {
+    "tjdevries/colorbuddy.nvim",
+    lazy = false,
+    config = function()
+      require('colorbuddy').setup()
+    end,
+  },
+  {
+    "svrana/neosolarized.nvim",
+    lazy = false,
+    config = function()
+      require('neosolarized').setup({
+        comment_italics = true,
+        background_set = false,
+    })
+    end,
   }
   -- You can also add new plugins here as well:
   -- Add plugins, the lazy syntax
